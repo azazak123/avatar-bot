0a25ff55b2480948f6324da68ecdf213efa84e36
azazak123 <azazaka2002@gmail.com>
2023-06-13 23:36:58 +0300

    docs: update welcome message

--- bin/command.ml
+++ bin/command.ml
@@ -6,8 +6,8 @@ let welcome token chat_id =
   let ( let* ) = Lwt.bind in
   let* resp, _body =
     Telegram_api.send_message token chat_id
-      "Hi ! This bot can ukrainianize your avatar ! Simply send picture and \
-       you will get it with ukrainian flag border !"
+      "Hi ! This bot can ukrainianize your avatar ! Just send a picture and \
+       you will receive its copy with the Ukrainian flag as a round border !"
   in
   resp |> Cohttp.Response.status |> Cohttp.Code.code_of_status
   |> (function code when code >= 200 && code < 300 -> true | _ -> false)
