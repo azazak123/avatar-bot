0191e01bfc3b16a09952ca1286247383a2df5cea
azazak123 <azazaka2002@gmail.com>
2023-06-14 19:38:12 +0300

    feat: add module interface for input parameters

--- lib/input_parameters.ml
+++ lib/input_parameters.ml
@@ -1,10 +1,10 @@
 open Ppx_deriving_yojson_runtime
 
-type chat = { id : int } [@@deriving yojson { strict = false }]
-type message = { chat : chat } [@@deriving yojson { strict = false }]
+type chat = { id : int } [@@deriving of_yojson { strict = false }]
+type message = { chat : chat } [@@deriving of_yojson { strict = false }]
 
 type t = { bot_token : string; message : message }
-[@@deriving yojson { strict = false }]
+[@@deriving of_yojson { strict = false }]
 
 let of_string str =
   let json = Yojson.Safe.from_string str in
---
+++ lib/input_parameters.mli
@@ -0,0 +1,5 @@
+type chat = { id : int }
+type message = { chat : chat }
+type t = { bot_token : string; message : message }
+
+val of_string : string -> (t, Error.t) result
