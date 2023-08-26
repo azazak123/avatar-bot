1793774b16df5caa079bc06207a742f58d604309
Volodymyr Antonov <azazaka2002@gmail.com>
2023-08-26 23:36:02 +0300

    feat(telegram_api): add sendPhoto method

--- lib/telegram_api.ml
+++ lib/telegram_api.ml
@@ -12,6 +12,32 @@ let send_message token chat_id text =
        "https://api.telegram.org/bot%s/sendMessage?chat_id=%d&text=%s" token
        chat_id text
 
+let send_photo token chat_id file =
+  Random.self_init ();
+  let boundary = ( ^ ) "-----" @@ Int64.to_string @@ Random.bits64 () in
+  let body =
+    Cohttp_lwt.Body.of_string
+    @@ Printf.sprintf
+         "--%s\r\n\
+          Content-Disposition: form-data; name=\"chat_id\"\r\n\
+          \r\n\
+          %d\r\n\
+          --%s\r\n\
+          Content-Disposition: form-data; name=\"photo\"; \
+          filename=\"image.png\"\r\n\
+          Content-Type: image/png\r\n\
+          \r\n\
+          %s\r\n\
+          --%s--\r\n"
+         boundary chat_id boundary file boundary
+  in
+  let headers =
+    Http.Header.add (Http.Header.init ()) "Content-Type"
+    @@ "multipart/form-data; boundary=" ^ boundary
+  in
+  Client.post ~body ~headers @@ Uri.of_string
+  @@ Printf.sprintf "https://api.telegram.org/bot%s/sendPhoto" token
+
 let get_file token file_id =
   let ( let* ) = Lwt.bind in
   let* _resp, body =
