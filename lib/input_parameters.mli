caacd593a88cbd1d39c5054c605fc655b69abd46
Volodymyr Antonov <azazaka2002@gmail.com>
2023-09-11 23:16:59 +0300

    feat: add document type

--- lib/input_parameters.mli
+++ lib/input_parameters.mli
@@ -1,6 +1,13 @@
 type chat = { id : int }
 type photo = { file_id : string }
-type message = { chat : chat; photo : photo array option }
+type document = { file_id : string; mime_type : string }
+
+type message = {
+  chat : chat;
+  photo : photo array option;
+  document : document option;
+}
+
 type t = { bot_token : string; message : message }
 
 val of_string : string -> t
