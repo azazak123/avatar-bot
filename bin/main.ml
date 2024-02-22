eabf651b47aa11af7e5300bbc19bfa7e4839de73
Volodymyr Antonov <azazaka2002@gmail.com>
2024-02-22 17:21:58 +0200

    feat: add Dream

--- bin/main.ml
+++ bin/main.ml
@@ -1,28 +1,27 @@
 open Avatar_bot
 
-let set_logging () =
-  Logs.set_reporter (Logs_fmt.reporter ());
-  Logs.set_level (Some Logs.Info)
-
 let main () =
-  set_logging ();
+  Dream.run @@ Dream.logger
+  @@ fun request ->
+  let ( let* ) = Lwt.bind in
 
-  let json_string = Sys.argv.(1) in
-
+  let* json_string = Dream.body request in
   Logs.info (fun m -> m "Input parameters: %s" json_string);
 
-  match
-    let parameters = Input_parameters.of_string json_string in
-    let command = Input_parameters.to_command parameters in
+  let parameters = Input_parameters.of_string json_string in
 
-    Logs.info (fun m -> m "Command: %s" (Command.show command));
+  let command = Input_parameters.to_command parameters in
+  
+  Logs.info (fun m -> m "Command: %s" (Command.show command));
 
-    Lwt_main.run @@ Command.process command
-  with
-  | Ok () -> ()
+  let* res = Command.process command in
+
+  Dream.json
+  @@
+  match res with
+  | Ok () -> Printf.sprintf {|{"body": { "result":"%b"} }|} true
   | Error err ->
       Logs.err (fun m -> m "Error: %s." (Error.show err));
-      Printf.printf {|{"body": { "result":"%b"} }|} false;
-      Stdlib.exit 1
+      Printf.sprintf {|{"body": { "result":"%b"} }|} false
 
 let () = main ()
